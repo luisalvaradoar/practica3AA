5857eab7afd5ec822f38ab8f016bbb617fa113ad
luisalvaradoar <43264748+luisalvaradoar@users.noreply.github.com>
2021-11-19 14:38:53 -0600

    Update Algorithm I - Day 3 - 1.py

--- A1/Algorithm I - Day 3 - 1.py
+++ A1/Algorithm I - Day 3 - 1.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
         count = 0
         for i in range(len(nums)):
                 if nums[i] == 0: 
